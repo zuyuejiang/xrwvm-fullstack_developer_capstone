667c09c5a1ffe7fabd7ada8c8f0180cc34b66faf
zuyuejiang <zuyue.jiang22@imperial.ac.uk>
2024-09-16 05:02:01 -0400

    Week2

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -61,7 +61,11 @@ ROOT_URLCONF = 'djangoproj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'frontend/static')],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'frontend/static'),
+            os.path.join(BASE_DIR, 'frontend/build'),
+            os.path.join(BASE_DIR, 'frontend/build/static'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -135,6 +139,8 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'frontend/static')
+    os.path.join(BASE_DIR, 'frontend/static'),
+    os.path.join(BASE_DIR, 'frontend/build'),
+    os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
 
--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -24,5 +24,7 @@ urlpatterns = [
     path('djangoapp/', include('djangoapp.urls')),
     path('about/', TemplateView.as_view(template_name="About.html")),
     path('', TemplateView.as_view(template_name="Home.html")),
+        path('login/', TemplateView.as_view(template_name="index.html")),
         path('contact/', TemplateView.as_view(template_name="Contact.html")),
+        path('register/', TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
