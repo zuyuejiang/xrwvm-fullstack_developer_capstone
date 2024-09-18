d9035120736ecf982873e6d29717c0c6cd6e8bdd
zuyuejiang <117514278+zuyuejiang@users.noreply.github.com>
2024-09-18 11:42:56 +0100

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,12 +30,15 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://ambervalenti-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+    'https://ambervalenti-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.'
+    'proxy.cognitiveclass.ai',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://ambervalenti-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+    'https://ambervalenti-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.'
+    'proxy.cognitiveclass.ai',
 ]
+
 
 
 REST_FRAMEWORK = {
